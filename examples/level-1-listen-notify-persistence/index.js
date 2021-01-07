b00c2e8430b4fc6ab1658b3b50ce1f07c1a2a548
Alex Tan Hong Pin <alextan220990@gmail.com>
2021-01-07 21:59:27 +0800

    chore: cleanup solution

--- examples/level-1-listen-notify-persistence/index.js
+++ examples/level-1-listen-notify-persistence/index.js
@@ -8,34 +8,34 @@ import createUsers from "../../user-store.js";
 // NOTE: Perform migrations. Don't do this in production.
 console.log("migrated", await migrate(db), "tables");
 
-// NOTE: We need a cron to retry failed events.
-// In multi-instance deployment, run this only if the instance is leader.
-const backgroundTask = cron.schedule("* * * * * *", () => {
-  // When the application starts, we run a cron to query the events in the
-  // `event` table to handle unprocessed events.
-  // We can do a batch query, and once it is completed, we can delete the
-  // unprocessed events.
-  console.log("running a task every second");
+// NOTE: Run a cron to retry failed events. In multi-instance deployment, run this only if the instance is the leader.
+const backgroundTask = cron.schedule("*/3 * * * * *", () => {
+  console.log("running a task every 3 seconds");
   //batch();
   stream();
 });
+
+function handle(event) {
+  console.log('processing', event)
+}
 
 async function batch() {
   console.log("running batch processing");
   // Process in transaction to ensure another running query won't process the
   // same work.
+  let lastId = 0;
   try {
     await db.query("BEGIN");
     const { rows: events } = await db.query(
-      "SELECT * FROM event LIMIT $1 FOR UPDATE",
-      [1000]
+      "SELECT * FROM event ORDER BY id LIMIT 1000 FOR UPDATE"
     );
-    if (!events.length) return;
-    let lastId = 0;
-    events.forEach(event => {
-      console.log("processing", event);
+    
+    // Handle or publish event.
+    for (let event of events) {
+      await handle(event)
       lastId = event.id;
-    });
+    }
+
     await db.query(`DELETE FROM event WHERE id <= $1`, [lastId]);
 
     await db.query("COMMIT");
@@ -45,64 +45,39 @@ async function batch() {
   }
 }
 
-async function stream() {
+async function stream(event) {
   console.log("running stream processing");
   try {
-    const countResult = await db.query(`SELECT count(*) AS count FROM event`);
-    const count = Number(countResult.rows[0].count);
-    if (!count) {
-      console.log("no event", count);
-      console.log("sleep");
-      return;
-    }
     await db.query("BEGIN");
     // Take any unprocessed event that has not yet been locked. We can also select multiple.
     const { rows } = await db.query(
-      "SELECT * FROM event LIMIT 1 FOR UPDATE SKIP LOCKED"
+      event?.id 
+        ? ...["SELECT * FROM event WHERE id = $1 LIMIT 1 FOR UPDATE NOWAIT", [event.id]]
+        : "SELECT * FROM event ORDER BY id LIMIT 1 FOR UPDATE SKIP LOCKED"
     );
-    const event = rows[0];
-    if (!event) {
+    const storedEvent = rows?.[0];
+    if (!storedEvent) {
       await db.query("COMMIT");
       return;
     }
-    // Process event.
-    console.log("processing unprocessed event", event);
+    await handle(event)
+    
     const result = await db.query("DELETE FROM event WHERE id = $1", [
-      event.id
+      storedEvent.id
     ]);
-    console.log("Deleted", result.rowCount);
     await db.query("COMMIT");
   } catch (e) {
     await db.query("ROLLBACK");
     throw e;
   }
 }
-
-db.on("notification", async ({ channel, payload }) => {
+    
+    
+// NOTE: Since we can have multiple db.on("notification"), we will receive
+// the message multiple times. This implementation is not robust.
+db.on("notification", ({ channel, payload }) => {
   const event = JSON.parse(payload);
-  // 1) Process the event.
-  // 2) Delete the event after processing.
-
-  // NOTE: Since we can have multiple db.on("notification"), we will receive
-  // the message multiple times. This implementation is not robust.
-  //await this.db.query(`DELETE FROM event WHERE id = $1`, [event.id])
-
-  // An alternative is to just wrap the operation in a transaction.
-  // But this means we still can only have one handler for notification.
-  try {
-    await db.query("BEGIN");
-    await db.query("SELECT * FROM event WHERE id = $1 FOR UPDATE", [event.id]);
-    // Process event.
-    console.log("processing event", event.id);
-    const result = await db.query("DELETE FROM event WHERE id = $1", [
-      event.id
-    ]);
-    console.log("Deleted", result.rowCount);
-    await db.query("COMMIT");
-  } catch (e) {
-    await db.query("ROLLBACK");
-    throw e;
-  }
+  stream(event)
 });
 
 await db.query("LISTEN person_created");
